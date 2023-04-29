769dfcfa0111f8b332afa9f97d2603a4594ed1cb
otis <olaliotis95@gmail.com>
2023-04-29 05:26:42 -0700

    0x05. Processes and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Create an infinite loop.
+ *
+ * Return: Always 0.
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates five (5) zombie processes.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+	pid_t zombie;
+
+	for (a = 0; a < 5; a++)
+	{
+		zombie = fork();
+		if (zombie == NULL)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+	infinite_while();
+	return (0);
+}
