dde14fc581e5a161749b8cf02539506972444570
otis <olaliotis95@gmail.com>
2023-05-04 04:58:16 -0700

    0x06. Regular expression

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# A Ruby script with only matching capital letters regular expressions.
+# A Ruby script with matching capital letters regular expressions.
 
-puts ARGV[0].scan(/A-Z/).join
+puts ARGV[0].scan(/[A-Z]/).join
